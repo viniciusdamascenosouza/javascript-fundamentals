8689af50ade523fb1efafa2f8df7a9e7e3b2170f
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-08 20:24:05 -0300

    the number data type

---
+++ dataTypes/numberType.js
@@ -0,0 +1,11 @@
+const firstSemesterGrade = 8.5;
+const secondSemesterGrade = 9;
+
+const grades = [firstSemesterGrade, secondSemesterGrade];
+
+const total = firstSemesterGrade + secondSemesterGrade;
+
+const average = total / grades.length;
+
+console.log("total", total.toFixed(2));
+console.log("average", average.toFixed(2));
