9b597ff4bfbf8e7ac72a49d2d9345424480ae840
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-09 19:30:20 -0300

    the string data type

---
+++ dataTypes/stringType.js
@@ -0,0 +1,6 @@
+const studentName = "Vinícius";
+const studentLastName = "Damasceno";
+const studentAge = 22;
+const presentation = `Hello, my name is ${studentName} ${studentLastName}, I'm ${studentAge} years old.`;
+
+console.log(presentation);
