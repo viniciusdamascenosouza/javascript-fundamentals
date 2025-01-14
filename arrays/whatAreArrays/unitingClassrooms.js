150486c93fb189d2fa0a64b14bd02bdf50249247
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-14 20:14:05 -0300

    unitingClassrooms.js

---
+++ arrays/whatAreArrays/unitingClassrooms.js
@@ -0,0 +1,6 @@
+const javaScriptClassroom = ['John', 'Jane', 'Marjorie', 'Bob'];
+const pythonClassroom = ['Alice', 'Juana', 'Charlie', 'David'];
+
+const unitingClassrooms = javaScriptClassroom + pythonClassroom
+
+console.log('unitingClassrooms:', javaScriptClassroom.concat(pythonClassroom));
