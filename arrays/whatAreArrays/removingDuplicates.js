29c124e2829f5734c89415551015df080ae57048
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-16 19:18:53 -0300

    removing duplicate names using set structure

---
+++ arrays/whatAreArrays/removingDuplicates.js
@@ -0,0 +1,15 @@
+const classroomStudents = [
+  "Ana",
+  "Marjorie",
+  "Maria",
+  "Maria",
+  "John",
+  "John",
+  "John",
+];
+
+const classroomStudentsUpdate = [...new Set(classroomStudents)];
+
+
+console.log(classroomStudents);
+console.log(classroomStudentsUpdate);
