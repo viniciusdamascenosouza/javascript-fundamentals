2fce3ef9a4150813ad8f47934a6b314851958c68
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-22 19:15:41 -0300

    understanding an object

---
+++ objects/object.js
@@ -0,0 +1,12 @@
+const cpfsList = ['12312312312', '123234435345'];
+
+const personInfo = ['name', 'Vinícius', 'age', 20];
+console.log(personInfo[0])
+
+const personInfoObject = {
+    name: 'Vinícius',
+    age: 20,
+    cpf: '12312312312',
+}
+
+console.log(`The client ${personInfoObject.name} is ${personInfoObject.age} years old and your cpf is ${personInfoObject.cpf}.`)
