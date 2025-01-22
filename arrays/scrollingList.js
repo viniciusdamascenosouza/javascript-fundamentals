ed238e9519efeaf39e9b36fc08dc69cde96cdb46
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-22 18:24:37 -0300

    scrolling through list with for classic

---
+++ arrays/scrollingList.js
@@ -0,0 +1,8 @@
+const grades = [10, 8.5, 5, 6.5, 8, 7.5];
+
+// first expression: initialization
+// second expression: condition
+// third expression: increment
+for (let i = 0; i < grades.length; i++) {
+    console.log(i, grades[i]);
+}
