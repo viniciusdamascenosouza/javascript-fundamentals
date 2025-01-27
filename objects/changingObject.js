c5c2bd5db437576894f06489686db91420070474
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-27 19:02:34 -0300

    Adding and changing objects

---
+++ objects/changingObject.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Vinícius",
+    age: 22,
+    cpf: "12312312312",
+    class: "Javascript",
+  };
+
+  student.telephone = "123456789"; 
+  console.log(student.telephone); // 123456789
+  console.log(student); // { name: 'Vinícius', age: 22, cpf: '12312312312', class: 'Javascript', telephone: '123456789' }
