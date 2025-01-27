a2f8d331c6f072da305a7251bb7b100ef505a509
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-27 18:55:56 -0300

    accessing objects with brackets

--- objects/objectStructure.js
+++ objects/objectStructure.js
@@ -1,14 +1,25 @@
 const student = {
-    name: 'Vinícius',
-    age: 22,
-    cpf: '12312312312',
-    class: 'Javascript',
+  name: "Vinícius",
+  age: 22,
+  cpf: "12312312312",
+  class: "Javascript",
+};
+
+console.log(student.name); // Vinícius;
+console.log(student.age); // 22;
+console.log(student.cpf); // 12312312312;
+console.log(student.class); // Javascript;
+
+console.log(
+  `The student ${student.name} is ${student.age} years old and is in the ${student.class} class.`
+); // The student Vinícius is 22 years old and is in the Javascript class.
+
+function showStudent(objectStudent, infoStudent) {
+  return objectStudent[infoStudent];
 }
 
-console.log(student.name) // Vinícius;
-console.log(student.age) // 22;
-console.log(student.cpf) // 12312312312;
-console.log(student.class) // Javascript;
+console.log(student['name']); // Vinícius
+console.log(student['cpf']); // 12312312312
 
-console.log(`The student ${student.name} is ${student.age} years old and is in the ${student.class} class.`); //
-
+console.log(showStudent(student, "name")); // Vinícius
+console.log(showStudent(student, "cpf")); // 12312312312
