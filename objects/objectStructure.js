b3f7abf4a083cce4ed38b785158f236cd95e54b0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-27 18:49:34 -0300

    accessing objects

---
+++ objects/objectStructure.js
@@ -0,0 +1,14 @@
+const student = {
+    name: 'Vinícius',
+    age: 22,
+    cpf: '12312312312',
+    class: 'Javascript',
+}
+
+console.log(student.name) // Vinícius;
+console.log(student.age) // 22;
+console.log(student.cpf) // 12312312312;
+console.log(student.class) // Javascript;
+
+console.log(`The student ${student.name} is ${student.age} years old and is in the ${student.class} class.`); //
+
