e1b9726f37567717d920f30ac08f454f2a894375
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-01-28 17:44:40 -0300

    values and data types

---
+++ objects/dataValues.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Vinícius",
+    age: 22,
+    cpf: "12312312312",
+    class: "Javascript",
+    telephones: [919999999, 929999999],
+  };
+
+  console.log(student.telephones) // [919999999, 929999999]
+  console.log(student.telephones[0]); // 919999999
+  console.log(student.telephones[1]); // 929999999
+
