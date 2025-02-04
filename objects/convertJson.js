70cb8357baf18df285f71b95a9babe0d47f2f573
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-02-04 19:47:27 -0300

    converting a json to string

---
+++ objects/convertJson.js
@@ -0,0 +1,6 @@
+const student = require("./student.json");
+
+const stringStudent = JSON.stringify(student);
+
+console.log(stringStudent);
+console.log(typeof stringStudent);
---
+++ objects/importJson.js
@@ -0,0 +1,7 @@
+const student = require("./student.json");
+
+console.log(student);
+console.log(typeof student);
+
+const keys = Object.keys(student);
+console.log(keys);
